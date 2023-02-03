11dfa9f426b182f1d96628ae224a16e33f76ce60
mithilesh <mithilesht@cloudthat.com>
2023-02-03 16:46:44 +0530

    created dynamic view using Spring Models

---
+++ src/main/java/com/abc/hellospringdemo/UserController.java
@@ -0,0 +1,19 @@
+package com.abc.hellospringdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class UserController {
+
+    @GetMapping("/users")
+    public String index(Model model) {
+        
+        model.addAttribute("message","Hello from Spring Controller!");
+        return "users";
+
+    }
+
+}
